3b80cc0c8758245e0e04086d26bd16e5ea0be22c
baier <evd.matvey@mail.ru>
2024-02-21 10:56:34 +0700

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
@@ -6,6 +7,14 @@ async function bootstrap() {
 
   app.enableCors({ credentials: true, origin: true });
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo list api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(4200);
 }
