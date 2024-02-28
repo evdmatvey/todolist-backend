c5cf6985f54c662e1f013a64b906c3f3985fa5f7
baier <evd.matvey@mail.ru>
2024-02-28 11:39:45 +0700

    Fix tag domain entity

--- src/domains/entities/__tests__/tag.entity.spec.ts
+++ src/domains/entities/__tests__/tag.entity.spec.ts
@@ -2,7 +2,7 @@ import { TagEntity } from '../tag.entity';
 
 describe('Tag Entity', () => {
   it('getTagData | should return correct object of tag data', () => {
-    const tagEntity = new TagEntity('id', 'title', '#000000');
+    const tagEntity = new TagEntity('id', 'user-id', 'title', '#000000');
     const expectedResult = { id: 'id', title: 'title', color: '#000000' };
     const tagData = tagEntity.getTagData();
 
@@ -10,14 +10,14 @@ describe('Tag Entity', () => {
   });
 
   it('isSought | should return true', () => {
-    const tagEntity = new TagEntity('id', 'title', '#000000');
+    const tagEntity = new TagEntity('id', 'user-id', 'title', '#000000');
     const result = tagEntity.isSought('ti');
 
     expect(result).toBe(true);
   });
 
   it('isSought | should return false', () => {
-    const tagEntity = new TagEntity('id', 'title', '#000000');
+    const tagEntity = new TagEntity('id', 'user-id', 'title', '#000000');
     const result = tagEntity.isSought('ii');
 
     expect(result).toBe(false);
--- src/domains/entities/tag.entity.ts
+++ src/domains/entities/tag.entity.ts
@@ -3,12 +3,18 @@ import { Color } from '@/domains/types';
 export class TagEntity {
   constructor(
     private readonly _id: string,
+    private readonly _userId: string,
     private readonly _title: string,
     private _color: Color,
   ) {}
 
   public getTagData() {
-    return { id: this._id, title: this._title, color: this._color };
+    return {
+      id: this._id,
+      userId: this._userId,
+      title: this._title,
+      color: this._color,
+    };
   }
 
   public isSought(searchQuery: string): boolean {
